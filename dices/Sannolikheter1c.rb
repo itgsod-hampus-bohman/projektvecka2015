47067616d896d9cb28ebdfa504821102055811aa
itgsod-hampus-bohman <hampus.bohman@elev.it-gymnasiet.se>
2015-03-24 09:40:27 +0100

    uppgift 1c

--- dices/Sannolikheter1c.rb
+++ dices/Sannolikheter1c.rb
@@ -8,9 +8,9 @@ def diceschema()
   (1..6).each do |dice1|
     (1..6).each do |dice2|
       if (dice1+dice2).abs == 7
-        print "#{dice1},#{dice2} ".red
-      else
         print "#{dice1},#{dice2} ".blue
+      else
+        print "#{dice1},#{dice2} ".red
       end
     end
     puts
