c908123649e3e28018aee4ed7581901882bae298
itgsod-hampus-bohman <hampus.bohman@elev.it-gymnasiet.se>
2015-03-24 09:35:09 +0100

    uppgift 1b

---
+++ dices/Sannolikheter1b.rb
@@ -0,0 +1,20 @@
+require 'colorize'
+
+def diceschema()
+
+  diffs = 0
+
+
+  (1..6).each do |dice1|
+    (1..6).each do |dice2|
+      if (dice1+dice2).abs == 12
+        print "#{dice1},#{dice2} ".red
+      else
+        print "#{dice1},#{dice2} ".blue
+      end
+    end
+    puts
+  end
+end
+
+diceschema()
